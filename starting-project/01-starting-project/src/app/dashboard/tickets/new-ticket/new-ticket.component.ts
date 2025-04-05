37cd352cdbcc0095937d4a74239147e618b04020
ghosh <ghosharghya918@gmail.com>
2025-04-05 23:22:57 +0530

    ang(17): Getting Access to Template and reset from

--- starting-project/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ starting-project/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
+import { HtmlParser } from '@angular/compiler';
 
 @Component({
   selector: 'app-new-ticket',
@@ -11,9 +12,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(titleEle: string, textEle: string): void {
+  onSubmit(titleEle: string, textEle: string, form:HTMLFormElement): void {
     console.log('SUBMITTED!');
     console.dir('SUBMITTED TITLE: ' + titleEle);
     console.dir('SUBMITTED TEXT: ' + textEle);
+    form.reset();
   }
 }
