aedf15b04118bc26cce55001a9ed6cdef77ba873
ghosh <ghosharghya918@gmail.com>
2025-04-05 20:00:24 +0530

    ang(17): Working with Template Variables

--- starting-project/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ starting-project/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,7 +11,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(): void {
+  onSubmit(titleEle: HTMLInputElement): void {
     console.log('SUBMITTED!');
+    console.dir('SUBMITTED TITLE: ' + titleEle.value);
   }
 }
