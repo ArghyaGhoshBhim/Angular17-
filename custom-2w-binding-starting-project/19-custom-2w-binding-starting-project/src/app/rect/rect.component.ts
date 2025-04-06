1f4058b56d30a859ccd89d52f7118a7554c26dd6
ghosh <ghosharghya918@gmail.com>
2025-04-06 21:45:57 +0530

    ang(17): 147. An Easier Way of Setting Up Custom Two-Way Binding

--- custom-2w-binding-starting-project/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ custom-2w-binding-starting-project/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,10 +9,15 @@ import { Component, input, output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  size = input.required<{ width: string; height: string }>();
-  sizeChange = output<{ width: string; height: string }>();
+  // size = input.required<{ width: string; height: string }>();
+  // sizeChange = output<{ width: string; height: string }>();
+  size = model.required<{ width: string; height: string }>();
   onReset() {
-    this.sizeChange.emit({
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100',
+    // });
+    this.size.set({
       width: '200',
       height: '100',
     });
