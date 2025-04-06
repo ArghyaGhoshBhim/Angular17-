dd782d265ddabdb00bcf27d80e94b49ad2264480
ghosh <ghosharghya918@gmail.com>
2025-04-06 19:04:17 +0530

    ang(17): 138. TypeScript & Type Models: Repetition

--- starting-project/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
+++ starting-project/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
-
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
   imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  styleUrl: './tickets.component.css',
 })
 export class TicketsComponent {
-
+  ticket: Ticket[] = [];
 }
