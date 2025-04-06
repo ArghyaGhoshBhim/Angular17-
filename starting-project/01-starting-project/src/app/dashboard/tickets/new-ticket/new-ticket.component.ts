bdbd191439dd51b4d272fda3bbc8d5657ccefa9d
ghosh <ghosharghya918@gmail.com>
2025-04-06 17:59:30 +0530

    ang(17): 134. A Closer Look at Decorator-based Queries & Lifecycle Hooks

--- starting-project/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ starting-project/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,14 +18,23 @@ import { HtmlParser } from '@angular/compiler';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  
+  //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  //Confirm view child loaded
+  ngAfterViewInit(): void {
+    console.log('After View INIT', this.form?.nativeElement);
+  }
+  ngOnInit(): void {
+    console.log('After ON INIT', this.form?.nativeElement);
+  }
   onSubmit(titleEle: string, textEle: string): void {
     console.log('SUBMITTED!');
     console.dir('SUBMITTED TITLE: ' + titleEle);
     console.dir('SUBMITTED TEXT: ' + textEle);
     // this.form?.nativeElement.reset();
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
   }
 }
