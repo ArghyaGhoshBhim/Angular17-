a4d6064aabf00661cf62318e549f18559fe4579e
ghosh <ghosharghya918@gmail.com>
2025-04-14 07:39:16 +0530

    ang(17): 182. Providing Services via the Element Injector

--- dependency-injection/01-starting-project/src/main.ts
+++ dependency-injection/01-starting-project/src/main.ts
@@ -4,6 +4,10 @@ import { AppComponent } from './app/app.component';
 import { TaskService } from './app/tasks/task.service';
 
 //platform injector this service will be definetly part of initial bundle
-bootstrapApplication(AppComponent, { providers: [TaskService] }).catch((err) =>
+// bootstrapApplication(AppComponent, { providers: [TaskService] }).catch((err) =>
+//   console.error(err)
+// );
+
+bootstrapApplication(AppComponent).catch((err) =>
   console.error(err)
 );
