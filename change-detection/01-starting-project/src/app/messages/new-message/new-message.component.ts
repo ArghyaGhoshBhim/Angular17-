2983f637b9db0d336097e03316c6e173034f2b7d
ghosh <ghosharghya918@gmail.com>
2025-04-15 07:12:57 +0530

    ang(17): Understanding the OnPush Strategy change the changedetion strategy in diff lavel and check log to understand the changes

--- change-detection/01-starting-project/src/app/messages/new-message/new-message.component.ts
+++ change-detection/01-starting-project/src/app/messages/new-message/new-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,8 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.css',
+  //If we make this onpush and type some thing then count component changedetection will run if we don't make change detection strategy onpush on counter component
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewMessageComponent {
   add = output<string>();
