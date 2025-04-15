79610be93d312cf2c0b6f92f79f4339fd9387a61
ghosh <ghosharghya918@gmail.com>
2025-04-15 06:29:43 +0530

    ang(17): Avoiding Zone Pollution

--- change-detection/01-starting-project/src/app/counter/counter.component.ts
+++ change-detection/01-starting-project/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,14 +9,29 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
-
+  private zone = inject(NgZone);
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
   }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    //This is a task which don't have any impact on the template, so here we don't need any change detection
+    //That's the reason we are using zone js and running this outside of the zone js
+    //After logging this console change detection will not run
+    //This is call avoiding zone polution
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Timer expired!');
+      }, 6000);
+    });
+  }
   onDecrement() {
     this.count.update((prevCount) => prevCount - 1);
   }
