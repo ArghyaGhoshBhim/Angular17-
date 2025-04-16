edf92fb94532dc15183d7b63d42bc40d509db55a
ghosh <ghosharghya918@gmail.com>
2025-04-16 22:26:12 +0530

    ang(17+):  Sending a GET Request To Fetch Data

--- http-request-response/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ http-request-response/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -12,7 +12,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   httpClient = inject(HttpClient);
+  private desRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscription = this.httpClient
+      .get<{ palces: Place[] }>('http://localhost:3000/places')
+      .subscribe({
+        next: (restData) => {
+          console.log(restData);
+        },
+      });
+
+    this.desRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
