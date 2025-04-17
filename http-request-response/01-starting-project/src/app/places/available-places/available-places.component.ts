ff3c87be3e81c6275703877272291d6eab9941da
ghosh <ghosharghya918@gmail.com>
2025-04-17 22:33:50 +0530

    ang(17+): Handling HTTP Errors

--- http-request-response/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ http-request-response/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,17 +16,29 @@ import { map } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
   httpClient = inject(HttpClient);
   private desRef = inject(DestroyRef);
   ngOnInit(): void {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((restData) => restData.places))
+      .pipe(
+        map((restData) => restData.places),
+        catchError((err) => {
+          console.log('Error', err);
+          return throwError(() => new Error('We have some internal issue'));
+        })
+      )
       .subscribe({
         next: (restData) => {
           this.places.set(restData);
           console.log(restData);
+        },
+        error: (error) => {
+          // console.log('Error: ', error);
+          // this.error.set('We have some internal issue');
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetching.set(false);
