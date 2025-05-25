ad3c064e8ac42898c4a0333f9a2d1b5ac8fc50da
ghosh <ghosharghya918@gmail.com>
2025-05-25 20:49:40 +0530

    fix(ang+) Exercise: Solution

--- forms-ang/01-starting-project/src/app/auth/signup/signup.component.ts
+++ forms-ang/01-starting-project/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.min(4)],
+    }),
+  });
+
+  onSubmit(): void {
+    this.form.reset();
+  }
+}
